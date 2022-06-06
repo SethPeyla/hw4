86a3f834cf80964e8c027ee91a159e02132af1e2
SethPeyla <seth.peyla@kellogg.northwestern.edu>
2022-06-06 00:07:57 +0000

    work on 06052022

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    redirect_to "/sessions/new"
+    if @user
+      if @user["password"] == params["password"]
+        flash["notice"] = "Welcome."
+        redirect_to "places"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
