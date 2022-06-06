fdda4c9247e591c2c5cdee0c1d0a97d65cba8029
SethPeyla <seth.peyla@kellogg.northwestern.edu>
2022-06-06 03:44:56 +0000

    work at 10:45pm

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by({ "email" => params["email"] })
     if @user
       if BCrypt::Password.new(@user["password"]) == params["password"]
-        cookies["user_id"] = @user["id"]
+        session["user_id"] = @user["id"]
         flash["notice"] = "Welcome."
         redirect_to "/places"
       else
